f489320bb582486565cf2cde9190c22c3c673e7c
BjornAlexCoL <75667280+BjornAlexCoL@users.noreply.github.com>
2022-02-08 13:01:03 +0100

    Updated division with 0

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -94,8 +94,8 @@ namespace Calculator
                 if (number2 == 0)
                 {
                     Console.ForegroundColor=ConsoleColor.Red;
-                    Console.WriteLine("Division with 0"!);
-                    Console.ResetColor();
+                    Console.WriteLine("\nDivision with 0"!);
+                    Console.ForegroundColor = Console.BackgroundColor;
                     return 0;
                 }
                 else
